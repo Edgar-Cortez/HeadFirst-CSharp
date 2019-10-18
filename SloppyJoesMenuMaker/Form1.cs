1d6de21c6917db6b209097cd96bf0b2110bbd540
Edgar Cortez-Castellano <eac0001@dcccd.edu>
2019-10-18 15:50:45 -0500

    Left off at the end of the Typing Game. Need to figure out how to make the game restart at the end of game over.

---
+++ SloppyJoesMenuMaker/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SloppyJoesMenuMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            MenuMaker menu = new MenuMaker()
+            {
+                Randomizer = new Random()
+            };
+
+            label1.Text = menu.GetMenuItem();
+            label2.Text = menu.GetMenuItem();
+            label3.Text = menu.GetMenuItem();
+            label4.Text = menu.GetMenuItem();
+            label5.Text = menu.GetMenuItem();
+            label6.Text = menu.GetMenuItem();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SloppyJoesMenuMaker/MenuMaker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SloppyJoesMenuMaker
+{
+    class MenuMaker
+    {
+        public Random Randomizer;
+
+        string[] Meats = { "Roast Beef", "Salami", "Turkey", "Chicken", "Ham", "Pastrami", "Meatball", "Sausage" };
+        string[] Condiments = { "Yellow Mustard", "Brown Mustard", "Honey Mustard",
+                    "Mayo", "Relish", "French Dressing", "Sriracha", "Buffalo Sauce", "Ketchup", "Vineager and Oil" };
+        string[] Breads = { "Rye", "White", "Wheat", "Pumpernickel", "Italian",
+                    "French", "Sourdough", "Pita", "Naan", "Hoagie"};
+
+        public string GetMenuItem()
+        {
+            string randomMeat = Meats[Randomizer.Next(Meats.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+            return randomMeat + " with " + randomCondiment + " on " + randomBread + " Bread.";
+        }
+    }
+}
